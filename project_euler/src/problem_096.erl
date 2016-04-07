3e40e5f2866eb30bfc728d848a9379ca523f2437
UshakovAV <std_string@mail.ru>
2016-04-07 18:42:30 +0500

    start implementation

--- project_euler/src/problem_096.erl
+++ project_euler/src/problem_096.erl
@@ -5,9 +5,27 @@
 
 -behaviour(numerical_task_behaviour).
 
+-define(SIDE_SIZE, 9).
+-define(GRID_SIZE, ?SIDE_SIZE * ?SIDE_SIZE).
+
+-record(grid_case, {name :: string(), grid :: array:array(integer())}).
+
 get_check_data() ->
     [{none, none}].
 
 prepare_data(_ModuleSourceDir, Input) -> Input.
 
 solve(none) -> none.
+
+convert_data([], Dest) -> Dest;
+convert_data([Description, Str1, Str2, Str3, Str4, Str5, Str6, Str7, Str8, Str9 | Rest], Dest) ->
+    Row1 = lists:map(fun(Char) -> Char - $0 end, Str1),
+    Row2 = lists:map(fun(Char) -> Char - $0 end, Str2),
+    Row3 = lists:map(fun(Char) -> Char - $0 end, Str3),
+    Row4 = lists:map(fun(Char) -> Char - $0 end, Str4),
+    Row5 = lists:map(fun(Char) -> Char - $0 end, Str5),
+    Row6 = lists:map(fun(Char) -> Char - $0 end, Str6),
+    Row7 = lists:map(fun(Char) -> Char - $0 end, Str7),
+    Row8 = lists:map(fun(Char) -> Char - $0 end, Str8),
+    Row9 = lists:map(fun(Char) -> Char - $0 end, Str9),
+    none.
