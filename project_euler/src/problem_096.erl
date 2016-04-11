003d670d776dcea79f3f8cd1ff8592e0dba7a78f
UshakovAV <std_string@mail.ru>
2016-04-11 18:32:36 +0500

    add some impl details

--- project_euler/src/problem_096.erl
+++ project_euler/src/problem_096.erl
@@ -1,7 +1,7 @@
 %% 
 
 -module(problem_096).
--export([get_check_data/0, prepare_data/2, solve/1]).
+-export([get_check_data/0, prepare_data/2, solve/1, solve/0]).
 
 -behaviour(numerical_task_behaviour).
 
@@ -11,13 +11,21 @@
 -record(grid_case, {name :: string(), grid :: array:array(integer())}).
 
 get_check_data() ->
-    [{none, none}].
+    [{"problem_096", none}].
 
-prepare_data(_ModuleSourceDir, Input) -> Input.
+prepare_data(ModuleSourceDir, Filename) ->
+    Strings = load_utils:read_strings(filename:join(ModuleSourceDir, Filename)),
+    convert_data(Strings, []).
 
-solve(none) -> none.
+solve(Data) ->
+    io:format("~p~n", [Data]).
 
-convert_data([], Dest) -> Dest;
+solve() ->
+    Strings = load_utils:read_strings("problem_096.dat"),
+    Data = convert_data(Strings, []),
+    io:format("~p~n", [Data]).
+
+convert_data([], Dest) -> lists:reverse(Dest);
 convert_data([Description, Str1, Str2, Str3, Str4, Str5, Str6, Str7, Str8, Str9 | Rest], Dest) ->
     Row1 = lists:map(fun(Char) -> Char - $0 end, Str1),
     Row2 = lists:map(fun(Char) -> Char - $0 end, Str2),
@@ -28,4 +36,14 @@ convert_data([Description, Str1, Str2, Str3, Str4, Str5, Str6, Str7, Str8, Str9 
     Row7 = lists:map(fun(Char) -> Char - $0 end, Str7),
     Row8 = lists:map(fun(Char) -> Char - $0 end, Str8),
     Row9 = lists:map(fun(Char) -> Char - $0 end, Str9),
-    none.
+    Grid = array:from_list(Row1 ++ Row2 ++ Row3 ++ Row4 ++ Row5 ++ Row6 ++ Row7 ++ Row8 ++ Row9),
+    convert_data(Rest, [#grid_case{name = Description, grid = Grid}] ++ Dest).
+
+%% TODO (std_string) : use such approach in all other cases
+get_element(X, Y, Grid) ->
+    Index = X * ?SIDE_SIZE + Y,
+    array:get(Index, Grid).
+
+set_element(X, Y, Value, Grid) ->
+    Index = X * ?SIDE_SIZE + Y,
+    array:set(Index, Value, Grid).
