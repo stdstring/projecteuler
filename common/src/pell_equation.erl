ca5af51d7c3d40c405be82274bc9601327e3023d
Ushakov Andrey <std_string@mail.ru>
2017-04-17 08:58:40 +0500

    added description of the algorithm

--- common/src/pell_equation.erl
+++ common/src/pell_equation.erl
@@ -10,7 +10,36 @@
 %% API functions
 %% ====================================================================
 
--spec find_first_solution(D :: integer(), C :: integer()) -> solution() | no_return().
+%% Algorithm (from http://mathworld.wolfram.com/PellEquation.html):
+%% Pell equation of the form x^2 - D * y^2 = 1 can be solved by finding the continued fraction [a(0), a(1), ...] of sqrt(D).
+%% Let ignore the trivial solution x = 1, y = 0.
+%% Let p(n) / q(n) denote the n-th convergent [a(0), a(1), ..., a(n)],
+%% then we will have solved Pell equation if we can find a convergent which obeys the identity p(n)^2 - D * q(n)^2 = (-1)^(n+1).
+%% Amazingly, this turns out to always be possible as a result of the fact that the continued fraction of a quadratic surd always becomes periodic at some term a(r+1),
+%% where a(r+1) = 2 * a(0), i.e., sqrt(D) = [a(0), a(1), ..., a(r), 2 * a(0), ...].
+%% To compute the continued fraction convergents to sqrt(D), use the usual recurrence relations:
+%% a(0) = [sqrt(D)]
+%% p(0) = a(0)
+%% p(1) = a(0) * a(1) + 1
+%% p(n) = a(n) * p(n-1) + p(n-2)
+%% q(0) = 1
+%% q(1) = a1
+%% q(n) = a(n) * q(n-1) + q(n-2)
+%% where [x] is the floor function. For reasons to be explained shortly, also compute the two additional quantities P(n) and Q(n) defined by
+%% P(0) = 0
+%% P(1) = a(0)
+%% Pn = a(n-1) * Q(n-1) - P(n-1)
+%% Q(0) = 1
+%% Q(1) = D - a(0) * a(0)
+%% Q(n) = (D - P(n) * P(n)) / Q(n-1)
+%% a(n) = [(a(0) + P(n)) / Q(n)]
+%% Let a(r+1) = 2 * a(0) be the term at which the continued fraction becomes periodic (which will always happen for a quadratic surd).
+%% For the Pell equation x^2 - D * y^2 = 1 with r odd, (-1)^(r+1) is positive and the solution in terms of smallest integers is x = p(r) and y = q(r),
+%% where p(r) / q(r) is the r-th convergent.
+%% If r is even, then (-1)^(r+1) is negative, but p(2r+1)^2 - D * q(2r+1)^2 = 1, so the solution in smallest integers is x = p(2r+1), y = q(2r+1).
+%% The equation x^2 - D * y^2 = -1 can be solved analogously to the equation with +1 on the right side if r is even, but has no solution if r is odd:
+%% x = p(r) and y = q(r) for r is even, no solution for r is odd
+-spec find_first_solution(D :: integer(), C :: integer()) -> solution() | 'undef' | no_return().
 find_first_solution(_D, C) when (C /= 1) and (C /= -1) -> error(not_supported);
 find_first_solution(D, C) ->
     check_d_value(D),
@@ -33,6 +62,18 @@ find_first_solution(D, C) ->
         (PrevIteration rem 2 == 0) and (C == 1) -> find_even_solution(D, A0, AN, {PN, PNPrev}, {QN, QNPrev}, PBigN, QBigN, Iteration, 2 * PrevIteration + 1)
     end.
 
+%% Algorithm (from http://mathworld.wolfram.com/PellEquation.html):
+%% Given one solution (x,y)=(p,q) (which can be found as above), a whole family of solutions can be found by taking each side to the n-th power,
+%% x^2 - D * y^2= (p^2 - D * q^2)^n = 1.
+%% Factoring gives the following:
+%% (x + sqrt(D) * y) * (x - sqrt(D) * y) = (p + sqrt(D) * q)^n * (p - sqrt(D) * q)^n
+%% and
+%% x + sqrt(D) * y  = (p + sqrt(D) * q)^n
+%% x - sqrt(D) * y  = (p - sqrt(D) * q)^n,
+%% which gives the family of solutions
+%% x = ((p + q * sqrt(D))^n + (p - q * sqrt(D))^n) / 2
+%% y = ((p + q * sqrt(D))^n - (p - q * sqrt(D))^n) / (2 * sqrt(D)).
+%% These solutions also hold for x^2 - D * y^2 = -1, except that n can take on only odd values.
 -spec find_n_solution(FirstSolution :: solution(), D :: pos_integer(), C :: integer(), N :: pos_integer()) -> solution() | no_return().
 find_n_solution(_FirstSolution, _D, C, _N) when (C /= 1) and (C /= -1) -> error(not_supported);
 find_n_solution(_FirstSolution, _D, C, N) when (C == -1) and (N rem 2 /= 1) -> error(badarg);
