4ee5cdcb0ce2bf24454dac41576faee3d7e68884
Ushakov Andrey <std_string@mail.ru>
2017-04-20 08:30:17 +0500

    added specs

--- project_euler/src/problem_001.erl
+++ project_euler/src/problem_001.erl
@@ -1,5 +1,6 @@
-%% If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-%% The sum of these multiples is 23.
+%% @author std-string
+
+%% If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 %% Find the sum of all the multiples of 3 or 5 below 1000.
 
 -module(problem_001).
@@ -7,16 +8,27 @@
 
 -behaviour(numerical_task_behaviour).
 
-get_check_data() ->
-    [{{3, 5, 1000-1}, 233168}].
+%% ====================================================================
+%% API functions
+%% ====================================================================
 
+-spec get_check_data() -> [{Input :: term(), Output :: term()}].
+get_check_data() ->
+    [{{3, 5, 10 - 1}, 23}, {{3, 5, 1000 - 1}, 233168}].
+
+-spec prepare_data(ModuleSourceDir :: string(), Input :: term()) -> term().
 prepare_data(_ModuleSourceDir, Input) -> Input.
 
+-spec solve(PreparedInput :: term()) -> term().
 solve({Number1, Number2, MaxNumber}) ->
     Sum1 = arith_progress(Number1, MaxNumber - MaxNumber rem Number1, MaxNumber div Number1),
     Sum2 = arith_progress(Number2, MaxNumber - MaxNumber rem Number2, MaxNumber div Number2),
-    Sum12 = arith_progress(Number1*Number2, MaxNumber - MaxNumber rem (Number1*Number2), MaxNumber div (Number1*Number2)),
-    trunc(Sum1 + Sum2 - Sum12).
+    Sum12 = arith_progress(Number1 * Number2, MaxNumber - MaxNumber rem (Number1 * Number2), MaxNumber div (Number1 * Number2)),
+    Sum1 + Sum2 - Sum12.
 
--spec arith_progress(A1 :: integer(), An :: integer(), N :: pos_integer()) -> integer().
-arith_progress(A1, An, N) -> N * (A1 + An) div 2. 
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+-spec arith_progress(A1 :: pos_integer(), An :: pos_integer(), N :: pos_integer()) -> pos_integer().
+arith_progress(A1, An, N) -> N * (A1 + An) div 2.
