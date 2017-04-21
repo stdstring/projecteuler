5433d046fcd2bf09da6f3c323f5003dc833c618c
Ushakov Andrey <std_string@mail.ru>
2017-04-21 14:11:31 +0500

    added specs + made refactoring

--- project_euler/src/problem_024.erl
+++ project_euler/src/problem_024.erl
@@ -1,3 +1,5 @@
+%% @author std-string
+
 %% A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
 %% If all of the permutations are listed numerically or alphabetically, we call it lexicographic order.
 %% The lexicographic permutations of 0, 1 and 2 are: 012, 021, 102, 120, 201, 210
@@ -8,14 +10,25 @@
 
 -behaviour(numerical_task_behaviour).
 
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+-spec get_check_data() -> [{Input :: term(), Output :: term()}].
 get_check_data() ->
     [{{4, 3}, "120"}, {{1000000, 10}, "2783915460"}].
 
+-spec prepare_data(ModuleSourceDir :: string(), Input :: term()) -> term().
 prepare_data(_ModuleSourceDir, Input) -> Input.
 
+-spec solve(PreparedInput :: term()) -> term().
 solve({_LexographicNumber, DigitCount}) when DigitCount > 10 ->
     throw(badarg);
 solve({LexographicNumber, DigitCount}) ->
     SourceDigits = lists:seq($0, $0 + DigitCount - 1),
     Alphabet = array:from_list(SourceDigits),
     permutations:get_permutation(LexographicNumber - 1, Alphabet).
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
--- project_euler/src/problem_028.erl
+++ project_euler/src/problem_028.erl
@@ -1,3 +1,5 @@
+%% @author std-string
+
 %% Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
 %%
 %% 21 22 23 24 25
@@ -15,12 +17,23 @@
 
 -behaviour(numerical_task_behaviour).
 
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+-spec get_check_data() -> [{Input :: term(), Output :: term()}].
 get_check_data() ->
     [{5, 101}, {1001, 669171001}].
 
+-spec prepare_data(ModuleSourceDir :: string(), Input :: term()) -> term().
 prepare_data(_ModuleSourceDir, Input) -> Input.
 
+-spec solve(PreparedInput :: term()) -> term().
 solve(SideSize) -> solve_impl(SideSize).
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
 
 %% solution:
 %% let SideSize - size of helix's side, then N - number of helix (beginning from 0) and SideSize = 2*N+1
@@ -28,6 +41,7 @@ solve(SideSize) -> solve_impl(SideSize).
 %% Volume (Sum of perimeters of lower helixes + central number 1) = 1 + Sum(8*k, k = 1..N) = 1 + 4*N*(N+1)
 %% For helix with number N corner's number will have follow numbers: V(N-1) + 2*N, V(N-1) + 4*N, V(N-1) + 6*N, V(N-1) + 8*N
 %% So, Sum of all diagonal numbers will be follows: 1 + Sum(4*V(k-1) + k*(2 + 4 +6 + 8), k = 1..N)
+-spec solve_impl(SideSize :: pos_integer()) -> pos_integer().
 solve_impl(SideSize) ->
     N = (SideSize - 1) div 2,
     1 + (4 * N) + (2 * N * (N + 1)) + (8 * N * (N + 1) * (2 * N + 1) div 3).
