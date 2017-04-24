217de7eeaa45e844d1b0bea570eac49587531341
Ushakov Andrey <std_string@mail.ru>
2017-04-24 11:08:34 +0500

    added specs

--- project_euler/src/problem_032.erl
+++ project_euler/src/problem_032.erl
@@ -15,8 +15,7 @@
 %% ====================================================================
 
 -spec get_check_data() -> [{Input :: term(), Output :: term()}].
-get_check_data() ->
-    [{none, 45228}].
+get_check_data() -> [{none, 45228}].
 
 -spec prepare_data(ModuleSourceDir :: string(), Input :: term()) -> term().
 prepare_data(_ModuleSourceDir, Input) -> Input.
