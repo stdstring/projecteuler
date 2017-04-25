5a377f5595306b6748e6750095e3a330a3a4810b
Ushakov Andrey <std_string@mail.ru>
2017-04-25 22:00:59 +0500

    added specs

--- project_euler/src/problem_058.erl
+++ project_euler/src/problem_058.erl
@@ -1,3 +1,5 @@
+%% @author std-string
+
 %% Starting with 1 and spiralling anticlockwise in the following way, a square spiral with side length 7 is formed.
 %%
 %% 37 36 35 34 33 32 31
@@ -17,19 +19,30 @@
 
 -behaviour(numerical_task_behaviour).
 
-get_check_data() ->
-    [{0.1, 26241}].
+-type process_result() :: {ProcessedCount :: non_neg_integer(), PrimeCount :: non_neg_integer()}.
 
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+-spec get_check_data() -> [{Input :: term(), Output :: term()}].
+get_check_data() -> [{0.1, 26241}].
+
+-spec prepare_data(ModuleSourceDir :: string(), Input :: term()) -> term().
 prepare_data(_ModuleSourceDir, Input) -> Input.
 
 %% Notes:
 %% Let S(N - 1) - last number from previous layer, L(N) - side size for layer N
 %% N1 = S(N - 1) + d * (L(N) - 1), where d = 1 => N1 = S(N - 1) + L(N) - 1
 %% Similarly, N2 =  N1 + L(N) - 1, N3 =  N2 + L(N) - 1, S(N) = N4 =  N3 + L(N) - 1
-solve(Ratio) ->
-    process_spiral(1, 1, Ratio, {1, 0}).
+-spec solve(PreparedInput :: term()) -> term().
+solve(Ratio) -> process_spiral(1, 1, Ratio, {1, 0}).
 
--spec process_spiral(LastNumber :: pos_integer(), SideSize :: pos_integer(), Ratio :: float(), {ProcessedCount :: non_neg_integer(), PrimeCount :: non_neg_integer()}) -> pos_integer().
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+-spec process_spiral(LastNumber :: pos_integer(), SideSize :: pos_integer(), Ratio :: float(), ProcessResult :: process_result()) -> pos_integer().
 process_spiral(_LastNumber, SideSize, Ratio, {ProcessedCount, PrimeCount}) when PrimeCount > 0, (PrimeCount / ProcessedCount) < Ratio -> SideSize;
 process_spiral(LastNumber, SideSize, Ratio, {ProcessedCount, PrimeCount}) ->
     NewSideSize = SideSize + 2,
@@ -41,8 +54,7 @@ process_spiral(LastNumber, SideSize, Ratio, {ProcessedCount, PrimeCount}) ->
     {NewProcessedCount, NewPrimeCount} = process_numbers([N1, N2, N3, N4], {ProcessedCount, PrimeCount}),
     process_spiral(N4, NewSideSize, Ratio, {NewProcessedCount, NewPrimeCount}).
 
--spec process_numbers(Numbers :: [pos_integer()], {ProcessedCount :: non_neg_integer(), PrimeCount :: non_neg_integer()}) ->
-    {ProcessedCount :: non_neg_integer(), PrimeCount :: non_neg_integer()}.
+-spec process_numbers(Numbers :: [pos_integer()], ProcessResult :: process_result()) -> process_result().
 process_numbers([], {ProcessedCount, PrimeCount}) -> {ProcessedCount, PrimeCount};
 process_numbers([Number | Numbers], {ProcessedCount, PrimeCount}) ->
     case number_dividers:is_prime(Number) of
