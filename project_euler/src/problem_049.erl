78df9ed880b70e09b717beddfc92df887f19bab5
Ushakov Andrey <std_string@mail.ru>
2017-04-25 09:38:31 +0500

    added specs

--- project_euler/src/problem_049.erl
+++ project_euler/src/problem_049.erl
@@ -1,3 +1,5 @@
+%% @author std-string
+
 %% The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways:
 %% (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are permutations of one another.
 %% There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property, but there is one other 4-digit increasing sequence.
@@ -13,11 +15,24 @@
 -define(SECOND_TERM_RANGE, [{1000, 5999}, {2000, 6499}, {3000, 6999}, {4000, 7499}, {5000, 7999}, {6000, 8499}, {7000, 8999}, {8000, 9499}, {9000, 9999}]).
 -define(KNOWN_RESULT, {1487, 4817, 8147}).
 
--record(task_state, {sieve = undefined :: 'undefined' | sets:set(pos_integer), second_term_ranges = undefined :: 'undefined' | array:array(pos_integer)}).
+%% TODO (std_string) : move into common
+-type digit() :: 0..9.
+-type digits() :: [digit()].
+-type number_range() :: 1000..9999.
+-type primes_seq() :: {FirstTerm :: number_range(), SecondTerm :: number_range(), ThirdTerm :: number_range()}.
+-type primes_set() :: sets:set(Prime :: pos_integer()).
+-type ranges_array() :: array:array({From :: pos_integer(), To :: pos_integer()}).
 
-get_check_data() ->
-    [{none, 296962999629}].
+-record(task_state, {sieve :: primes_set(), second_term_ranges :: ranges_array()}).
 
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+-spec get_check_data() -> [{Input :: term(), Output :: term()}].
+get_check_data() -> [{none, 296962999629}].
+
+-spec prepare_data(ModuleSourceDir :: string(), Input :: term()) -> term().
 prepare_data(_ModuleSourceDir, Input) -> Input.
 
 %% second term dy first term:
@@ -30,6 +45,7 @@ prepare_data(_ModuleSourceDir, Input) -> Input.
 %% 7) for [7000, 7999] - [7000, 8999]
 %% 8) for [8000, 8999] - [8000, 9499]
 %% 9) for [9000, 9999] - [9000, 9999]
+-spec solve(PreparedInput :: term()) -> term().
 solve(none) ->
     Sieve = lists:dropwhile(fun(Number) -> Number < ?MIN_NUMBER end, eratos_sieve:get_primes(?MAX_NUMBER)),
     SieveSet = sets:from_list(Sieve),
@@ -41,7 +57,11 @@ solve(none) ->
     [Result] = lists:filtermap(FilterMapFun, ProcessResults),
     Result.
 
--spec process_prime(Prime :: 1000..9999, State :: #task_state{}) -> [{FirstTerm :: 1000..9999, SecondTerm :: 1000..9999, ThirdTerm :: 1000..9999}].
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+-spec process_prime(Prime :: number_range(), State :: #task_state{}) -> [primes_seq()].
 process_prime(Prime, State) ->
     [D1, D2, D3, D4] = numbers:get_digits(Prime),
     SieveSet = State#task_state.sieve,
@@ -59,7 +79,7 @@ process_prime(Prime, State) ->
                                        sets:is_element(ThirdTerm, SieveSet),
                                        check_digits([D1, D2, D3, D4], numbers:get_digits(ThirdTerm))].
 
--spec generate_numbers(D1 :: 0..9, D2 :: 0..9, D3 :: 0..9, D4 :: 0..9) -> [[0..9]].
+-spec generate_numbers(D1 :: digit(), D2 :: digit(), D3 :: digit(), D4 :: digit()) -> [digits()].
 generate_numbers(D1, D, D, D) ->
     [[D, D1, D, D], [D, D, D1, D], [D, D, D, D1]];
 generate_numbers(D, D2, D, D) ->
@@ -69,17 +89,77 @@ generate_numbers(D, D, D3, D) ->
 generate_numbers(D, D, D, D4) ->
     [[D4, D, D, D], [D, D4, D, D], [D, D, D4, D]];
 generate_numbers(D1, D2, D, D) ->
-    [[D1, D, D2, D], [D1, D, D, D2], [D2, D1, D, D], [D2, D, D1, D], [D2, D, D, D1], [D, D1, D2, D], [D, D1, D, D2], [D, D2, D1, D], [D, D2, D, D1], [D, D, D1, D2], [D, D, D2, D1]];
+    [[D1, D, D2, D],
+     [D1, D, D, D2],
+     [D2, D1, D, D],
+     [D2, D, D1, D],
+     [D2, D, D, D1],
+     [D, D1, D2, D],
+     [D, D1, D, D2],
+     [D, D2, D1, D],
+     [D, D2, D, D1],
+     [D, D, D1, D2],
+     [D, D, D2, D1]];
 generate_numbers(D1, D, D3, D) ->
-    [[D1, D3, D, D], [D1, D, D, D3], [D3, D1, D, D], [D3, D, D1, D], [D3, D, D, D1], [D, D1, D3, D], [D, D1, D, D3], [D, D3, D1, D], [D, D3, D, D1], [D, D, D1, D3], [D, D, D3, D1]];
+    [[D1, D3, D, D],
+     [D1, D, D, D3],
+     [D3, D1, D, D],
+     [D3, D, D1, D],
+     [D3, D, D, D1],
+     [D, D1, D3, D],
+     [D, D1, D, D3],
+     [D, D3, D1, D],
+     [D, D3, D, D1],
+     [D, D, D1, D3],
+     [D, D, D3, D1]];
 generate_numbers(D1, D, D, D4) ->
-    [[D1, D4, D, D], [D1, D, D4, D], [D4, D1, D, D], [D4, D, D1, D], [D4, D, D, D1], [D, D1, D4, D], [D, D1, D, D4], [D, D4, D1, D], [D, D4, D, D1], [D, D, D1, D4], [D, D, D4, D1]];
+    [[D1, D4, D, D],
+     [D1, D, D4, D],
+     [D4, D1, D, D],
+     [D4, D, D1, D],
+     [D4, D, D, D1],
+     [D, D1, D4, D],
+     [D, D1, D, D4],
+     [D, D4, D1, D],
+     [D, D4, D, D1],
+     [D, D, D1, D4],
+     [D, D, D4, D1]];
 generate_numbers(D, D2, D3, D) ->
-    [[D2, D3, D, D], [D2, D, D3, D], [D2, D, D, D3], [D3, D2, D, D], [D3, D, D2, D], [D3, D, D, D2], [D, D2, D, D3], [D, D3, D2, D], [D, D3, D, D2], [D, D, D2, D3], [D, D, D3, D2]];
+    [[D2, D3, D, D],
+     [D2, D, D3, D],
+     [D2, D, D, D3],
+     [D3, D2, D, D],
+     [D3, D, D2, D],
+     [D3, D, D, D2],
+     [D, D2, D, D3],
+     [D, D3, D2, D],
+     [D, D3, D, D2],
+     [D, D, D2, D3],
+     [D, D, D3, D2]];
 generate_numbers(D, D2, D, D4) ->
-    [[D2, D4, D, D], [D2, D, D4, D], [D4, D2, D, D], [D4, D, D2, D], [D4, D, D, D2], [D, D2, D4, D], [D, D2, D, D4], [D, D4, D2, D], [D, D4, D, D2], [D, D, D2, D4], [D, D, D4, D2]];
+    [[D2, D4, D, D],
+     [D2, D, D4, D],
+     [D4, D2, D, D],
+     [D4, D, D2, D],
+     [D4, D, D, D2],
+     [D, D2, D4, D],
+     [D, D2, D, D4],
+     [D, D4, D2, D],
+     [D, D4, D, D2],
+     [D, D, D2, D4],
+     [D, D, D4, D2]];
 generate_numbers(D, D, D3, D4) ->
-    [[D3, D4, D, D], [D3, D, D4, D], [D3, D, D, D4], [D4, D3, D, D], [D4, D, D3, D], [D4, D, D, D3], [D, D3, D4, D], [D, D3, D, D4], [D, D4, D3, D], [D, D4, D, D3], [D, D, D4, D3]];
+    [[D3, D4, D, D],
+     [D3, D, D4, D],
+     [D3, D, D, D4],
+     [D4, D3, D, D],
+     [D4, D, D3, D],
+     [D4, D, D, D3],
+     [D, D3, D4, D],
+     [D, D3, D, D4],
+     [D, D4, D3, D],
+     [D, D4, D, D3],
+     [D, D, D4, D3]];
 generate_numbers(D1, D2, D3, D4) ->
     [[D1, D2, D4, D3],
      [D1, D3, D2, D4],
@@ -105,7 +185,7 @@ generate_numbers(D1, D2, D3, D4) ->
      [D4, D3, D1, D2],
      [D4, D3, D2, D1]].
 
--spec check_digits(Expected :: [0..9], Actual :: [0..9]) -> boolean().
+-spec check_digits(Expected :: digits(), Actual :: digits()) -> boolean().
 check_digits([D1, D2, D3, D4], [D1, D2, D3, D4]) -> true;
 check_digits([D1, D2, D3, D4], [D1, D2, D4, D3]) -> true;
 check_digits([D1, D2, D3, D4], [D1, D3, D2, D4]) -> true;
--- project_euler/src/problem_052.erl
+++ project_euler/src/problem_052.erl
@@ -1,3 +1,5 @@
+%% @author std-string
+
 %% It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
 %% Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
 
@@ -7,13 +9,26 @@
 
 -behaviour(numerical_task_behaviour).
 
-get_check_data() ->
-    [{none, 142857}].
+-type search_result() :: {'true', Number :: pos_integer()} | 'false'.
 
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+-spec get_check_data() -> [{Input :: term(), Output :: term()}].
+get_check_data() -> [{none, 142857}].
+
+-spec prepare_data(ModuleSourceDir :: string(), Input :: term()) -> term().
 prepare_data(_ModuleSourceDir, Input) -> Input.
 
+-spec solve(PreparedInput :: term()) -> term().
 solve(none) -> search(1).
 
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+-spec search(DigitCount :: pos_integer()) -> pos_integer().
 search(DigitCount) ->
     MinNumber = numbers:power(10, DigitCount - 1),
     MaxNumber = (numbers:power(10, DigitCount) - 1) div 6,
@@ -22,6 +37,7 @@ search(DigitCount) ->
         false -> search(DigitCount - 1)
     end.
 
+-spec search(CurrentNumber :: pos_integer(), MaxNumber :: pos_integer()) -> search_result().
 search(CurrentNumber, MaxNumber) when MaxNumber < CurrentNumber -> false;
 search(CurrentNumber, MaxNumber) ->
     case check_number(CurrentNumber, get_result_numbers(CurrentNumber)) of
@@ -29,9 +45,10 @@ search(CurrentNumber, MaxNumber) ->
         false -> search(MaxNumber, CurrentNumber + 1)
     end.
 
+-spec check_number(Number :: pos_integer(), ExpectedResult :: [pos_integer()]) -> boolean().
 check_number(Number, ExpectedResult) ->
     NumberDigits = lists:sort(numbers:get_digits(Number)),
     lists:all(fun(ExpectedNumber) -> NumberDigits == lists:sort(ExpectedNumber) end, ExpectedResult).
 
-get_result_numbers(Number) ->
-    [numbers:get_digits(Number * Factor) || Factor <- lists:seq(2, 6)].
+-spec get_result_numbers(Number :: pos_integer()) -> [pos_integer()].
+get_result_numbers(Number) -> [numbers:get_digits(Number * Factor) || Factor <- lists:seq(2, 6)].
