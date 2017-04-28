5917a5d39b934687faec5e82d50bf53b5ce4cf43
Ushakov Andrey <std_string@mail.ru>
2017-04-28 13:05:45 +0500

    added calculation of binominal coefficients into numbers module + tests on this

--- common_tests/src/numbers_tests.erl
+++ common_tests/src/numbers_tests.erl
@@ -8,18 +8,23 @@
 %% Test functions
 %% ====================================================================
 
+power_error_test_() ->
+    [{"2.1^2", ?_assertError(badarg, numbers:power(2.1, 2))},
+     {"2^2.1", ?_assertError(badarg, numbers:power(2, 2.1))},
+     {"2^-1", ?_assertError(badarg, numbers:power(2, -1))},
+     {"0^0", ?_assertError(badarg, numbers:power(0, 0))}].
+
 power_test_() ->
-    [{"2.0^2", ?_assertError(function_clause, numbers:power(2.0, 2))},
-     {"2^-1", ?_assertThrow(badarg, numbers:power(2, -1))},
-     {"0^0", ?_assertThrow(badarg, numbers:power(0, 0))},
-     {"2^0", ?_assertEqual(1, numbers:power(2, 0))},
+    [{"2^0", ?_assertEqual(1, numbers:power(2, 0))},
      {"-2^3", ?_assertEqual(-8, numbers:power(-2, 3))},
      {"2^3", ?_assertEqual(8, numbers:power(2, 3))}].
 
+factorial_error_test_() ->
+    [{"2.0!", ?_assertError(badarg, numbers:factorial(2.0))},
+     {"-1!", ?_assertError(badarg, numbers:factorial(-1))}].
+
 factorial_test_() ->
-    [{"2.0!", ?_assertError(function_clause, numbers:factorial(2.0))},
-     {"-1!", ?_assertError(function_clause, numbers:factorial(-1))},
-     {"0!", ?_assertEqual(1, numbers:factorial(0))},
+    [{"0!", ?_assertEqual(1, numbers:factorial(0))},
      {"1!", ?_assertEqual(1, numbers:factorial(1))},
      {"2!", ?_assertEqual(2, numbers:factorial(2))},
      {"3!", ?_assertEqual(6, numbers:factorial(3))},
@@ -27,8 +32,7 @@ factorial_test_() ->
      {"5!", ?_assertEqual(120, numbers:factorial(5))}].
 
 get_digits_base10_test_() ->
-    [{"get digits for 2.0", ?_assertError(function_clause, numbers:get_digits(2.0))},
-     {"get digits for 0", ?_assertEqual([0], numbers:get_digits(0))},
+    [{"get digits for 0", ?_assertEqual([0], numbers:get_digits(0))},
      {"get digits for 1", ?_assertEqual([1], numbers:get_digits(1))},
      {"get digits for 7", ?_assertEqual([7], numbers:get_digits(7))},
      {"get digits for 10", ?_assertEqual([1, 0], numbers:get_digits(10))},
@@ -37,15 +41,16 @@ get_digits_base10_test_() ->
      {"get digits for 900", ?_assertEqual([9, 0, 0], numbers:get_digits(900))}].
 
 get_digits_base2_test_() ->
-    [{"get digits for 2.0", ?_assertError(function_clause, numbers:get_digits(2.0, 2))},
-     {"get digits for 0", ?_assertEqual([0], numbers:get_digits(0, 2))},
+    [{"get digits for 0", ?_assertEqual([0], numbers:get_digits(0, 2))},
      {"get digits for 1", ?_assertEqual([1], numbers:get_digits(1, 2))},
      {"get digits for 3", ?_assertEqual([1, 1], numbers:get_digits(3, 2))},
      {"get digits for 5", ?_assertEqual([1, 0, 1], numbers:get_digits(5, 2))}].
 
 get_digits_error_test_() ->
-    [{"get digits for base < 2", ?_assertThrow(badarg, numbers:get_digits(0, 1))},
-     {"get digits for base > 10", ?_assertThrow(notsup, numbers:get_digits(0, 11))}].
+    [{"get digits for 2.0 on the base 10", ?_assertError(badarg, numbers:get_digits(2.0))},
+     {"get digits for 2.0 on the base 2", ?_assertError(badarg, numbers:get_digits(2.0, 2))},
+     {"get digits for base < 2", ?_assertError(badarg, numbers:get_digits(0, 1))},
+     {"get digits for base > 10", ?_assertError(badarg, numbers:get_digits(0, 11))}].
 
 get_number_base10_test_() ->
     [{"get digits for [0]", ?_assertEqual(0, numbers:get_number([0]))},
@@ -61,9 +66,33 @@ get_number_base2_test_() ->
      {"get digits for [1, 0, 1]", ?_assertEqual(5, numbers:get_number([1, 0, 1], 2))}].
 
 get_number_error_test_() ->
-    [{"get number for base < 2", ?_assertThrow(badarg, numbers:get_number([0], 1))},
-     {"get number for base > 10", ?_assertThrow(notsup, numbers:get_number([0], 11))},
-     {"get number for incorrect digit", ?_assertThrow(badarg, numbers:get_number([8], 2))}].
+    [{"get number for base < 2", ?_assertError(badarg, numbers:get_number([0], 1))},
+     {"get number for base > 10", ?_assertError(badarg, numbers:get_number([0], 11))},
+     {"get number for incorrect digit", ?_assertError(badarg, numbers:get_number([8], 2))}].
+
+calc_binomial_coeff_error_test_() ->
+    [{"calc_binomial_coeff(2.0, 1)", ?_assertError(badarg, numbers:calc_binomial_coeff(2.0, 1))},
+     {"calc_binomial_coeff(2, 1.0)", ?_assertError(badarg, numbers:calc_binomial_coeff(2, 1.0))},
+     {"calc_binomial_coeff(-1, 666)", ?_assertError(badarg, numbers:calc_binomial_coeff(-1, 666))}].
+
+calc_binomial_coeff_test_() ->
+    [{"calc_binomial_coeff(0, 0)", ?_assertEqual(1, numbers:calc_binomial_coeff(0, 0))},
+     {"calc_binomial_coeff(1, 0)", ?_assertEqual(1, numbers:calc_binomial_coeff(1, 0))},
+     {"calc_binomial_coeff(1, 1)", ?_assertEqual(1, numbers:calc_binomial_coeff(1, 1))},
+     {"calc_binomial_coeff(2, 0)", ?_assertEqual(1, numbers:calc_binomial_coeff(2, 0))},
+     {"calc_binomial_coeff(2, 1)", ?_assertEqual(2, numbers:calc_binomial_coeff(2, 1))},
+     {"calc_binomial_coeff(2, 2)", ?_assertEqual(1, numbers:calc_binomial_coeff(2, 2))},
+     {"calc_binomial_coeff(3, 0)", ?_assertEqual(1, numbers:calc_binomial_coeff(3, 0))},
+     {"calc_binomial_coeff(3, 1)", ?_assertEqual(3, numbers:calc_binomial_coeff(3, 1))},
+     {"calc_binomial_coeff(3, 2)", ?_assertEqual(3, numbers:calc_binomial_coeff(3, 2))},
+     {"calc_binomial_coeff(3, 3)", ?_assertEqual(1, numbers:calc_binomial_coeff(3, 3))},
+     {"calc_binomial_coeff(4, 0)", ?_assertEqual(1, numbers:calc_binomial_coeff(4, 0))},
+     {"calc_binomial_coeff(4, 1)", ?_assertEqual(4, numbers:calc_binomial_coeff(4, 1))},
+     {"calc_binomial_coeff(4, 2)", ?_assertEqual(6, numbers:calc_binomial_coeff(4, 2))},
+     {"calc_binomial_coeff(4, 3)", ?_assertEqual(4, numbers:calc_binomial_coeff(4, 3))},
+     {"calc_binomial_coeff(4, 4)", ?_assertEqual(1, numbers:calc_binomial_coeff(4, 4))},
+     {"calc_binomial_coeff(4, -1)", ?_assertEqual(0, numbers:calc_binomial_coeff(4, -1))},
+     {"calc_binomial_coeff(4, 666)", ?_assertEqual(0, numbers:calc_binomial_coeff(4, 666))}].
 
 %% ====================================================================
 %% Internal functions
