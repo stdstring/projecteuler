8e07c40bec1708b97e421271bb0903a333870b19
Ushakov Andrey <std_string@mail.ru>
2018-01-04 11:01:43 +0500

    removed grid_helper module & grid_def header file

--- common/include/grid_def.hrl
+++
@@ -1,6 +0,0 @@
-%% @author std-string
-
-%% grid definitions
--type grid_type() :: array:array(array:array(term())).
--type grid_type(Element) :: array:array(array:array(Element)).
--type point_type() :: {Row :: pos_integer(), Column :: pos_integer()}.
