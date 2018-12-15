2f3ec614c69e200b4bbd6d9287041a8aa32e2366
stdstring <std_string@mail.ru>
2018-12-15 18:43:16 +0500

    reimplemented solution of problem 002

--- projecteuler.fsharp/ProjectEulerTasks/Utils/SolutionUtils.fs
+++ projecteuler.fsharp/ProjectEulerTasks/Utils/SolutionUtils.fs
@@ -9,7 +9,7 @@ type SolutionUtils =
     static member public CheckSolution<'TResult>(timeLimit: int, expectedAnswer: 'TResult, solver: unit -> 'TResult) =
         let stopwatch = Stopwatch.StartNew()
         let result = solver ()
-        Assert.AreEqual(expectedAnswer, result)
         stopwatch.Stop()
         printfn "Execution time is %i ms" stopwatch.ElapsedMilliseconds
+        Assert.AreEqual(expectedAnswer, result)
         Assert.LessOrEqual(stopwatch.ElapsedMilliseconds, timeLimit)
