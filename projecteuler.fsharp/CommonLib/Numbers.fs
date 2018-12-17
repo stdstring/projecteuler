58f99b161cc6e84abbb8e6792b41dca7e09ca3d8
stdstring <std_string@mail.ru>
2018-12-17 20:31:56 +0500

    reimplemented solution of problem 004

---
+++ projecteuler.fsharp/CommonLib/Numbers.fs
@@ -0,0 +1,70 @@
+﻿namespace CommonLib
+
+open System
+
+[<AbstractClass; Sealed>]
+type Numbers =
+
+    static member public GetDigits(number: int) =
+        Numbers.GetDigits(number, 10)
+
+    static member public GetDigits(number: int64) =
+        Numbers.GetDigits(number, 10)
+
+    static member public GetDigits(number: bigint) =
+        Numbers.GetDigits(number, 10)
+
+    static member public GetDigits(number: int, radix: int) =
+        Numbers.CheckRadix(radix)
+        match number with
+        | _ when number < 0 -> raise (ArgumentOutOfRangeException("number"))
+        | _ -> Numbers.GetDigitsImpl(number, radix, [])
+
+    static member public GetDigits(number: int64, radix: int) =
+        Numbers.CheckRadix(radix)
+        match number with
+        | _ when number < 0L -> raise (ArgumentOutOfRangeException("number"))
+        | _ -> Numbers.GetDigitsImpl(number, radix, [])
+
+    static member public GetDigits(number: bigint, radix: int) =
+        Numbers.CheckRadix(radix)
+        match number with
+        | _ when number < 0I -> raise (ArgumentOutOfRangeException("number"))
+        | _ -> Numbers.GetDigitsImpl(number, radix, [])
+
+    static member public GetNumber(digits: List<int>) =
+        Numbers.GetNumber(digits, 10)
+
+    static member public GetNumber(digits: List<int>, radix: int) =
+        Numbers.CheckDigits(digits, radix)
+        Numbers.CheckRadix(radix)
+        let radixBig = bigint radix
+        digits |> List.fold (fun number digit -> number * radixBig + (bigint digit)) 0I
+
+    static member private CheckRadix(radix: int) =
+        match radix with
+        | _ when radix < 2 -> raise (ArgumentOutOfRangeException("radix"))
+        | _ when (2 <= radix) && (radix <= 10) -> ()
+        | _ -> raise (NotSupportedException())
+
+    static member private CheckDigits(digits: List<int>, radix: int) =
+        match (digits |> List.exists (fun digit -> (digit < 0) || (digit >= radix))) with
+        | true -> raise (ArgumentOutOfRangeException("digits"))
+        | false -> ()
+
+    static member private GetDigitsImpl(number: int, radix: int, digits: List<int>) =
+        match number with
+        | _ when number < radix -> number :: digits
+        | _ -> Numbers.GetDigitsImpl(number / radix, radix, (number % radix) :: digits)
+
+    static member private GetDigitsImpl(number: int64, radix: int, digits: List<int>) =
+        let radix64 = int64 radix
+        match number with
+        | _ when number < radix64 -> (int number) :: digits
+        | _ -> Numbers.GetDigitsImpl(number / radix64, radix, (number % radix64 |> int) :: digits)
+
+    static member private GetDigitsImpl(number: bigint, radix: int, digits: List<int>) =
+        let radixBig = bigint radix
+        match number with
+        | _ when number < radixBig -> (int number) :: digits
+        | _ -> Numbers.GetDigitsImpl(number / radixBig, radix, (number % radixBig |> int) :: digits)
---
+++ projecteuler.fsharp/CommonLibTests/NumbersTests.fs
@@ -0,0 +1,287 @@
+﻿namespace CommonLibTests
+
+open NUnit.Framework
+open System
+open CommonLib
+
+[<TestFixture>]
+type NumbersTests() =
+
+    let maxint = Int32.MaxValue
+    let maxint64 = Int64.MaxValue
+
+    [<Test>]
+    member public this.GetDigits() =
+        // int
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(-1) |> ignore) |> ignore
+        Assert.AreEqual([0], Numbers.GetDigits(0))
+        Assert.AreEqual([1], Numbers.GetDigits(1))
+        Assert.AreEqual([5], Numbers.GetDigits(5))
+        Assert.AreEqual([1; 0], Numbers.GetDigits(10))
+        Assert.AreEqual([1; 1], Numbers.GetDigits(11))
+        Assert.AreEqual([2; 9], Numbers.GetDigits(29))
+        Assert.AreEqual([1; 2; 3; 4; 5; 6; 7; 8; 9; 0], Numbers.GetDigits(1234567890))
+        Assert.AreEqual([2; 1; 4; 7; 4; 8; 3; 6; 4; 7], Numbers.GetDigits(maxint))
+        // int64
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(-1L) |> ignore) |> ignore
+        Assert.AreEqual([0], Numbers.GetDigits(0L))
+        Assert.AreEqual([1], Numbers.GetDigits(1L))
+        Assert.AreEqual([5], Numbers.GetDigits(5L))
+        Assert.AreEqual([1; 0], Numbers.GetDigits(10L))
+        Assert.AreEqual([1; 1], Numbers.GetDigits(11L))
+        Assert.AreEqual([2; 9], Numbers.GetDigits(29L))
+        Assert.AreEqual([1; 2; 3; 4; 5; 6; 7; 8; 9; 0], Numbers.GetDigits(1234567890L))
+        Assert.AreEqual([2; 1; 4; 7; 4; 8; 3; 6; 4; 7], Numbers.GetDigits(int64 maxint))
+        Assert.AreEqual([9; 2; 2; 3; 3; 7; 2; 0; 3; 6; 8; 5; 4; 7; 7; 5; 8; 0; 7], Numbers.GetDigits(maxint64))
+        // bigint
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(-1I) |> ignore) |> ignore
+        Assert.AreEqual([0], Numbers.GetDigits(0I))
+        Assert.AreEqual([1], Numbers.GetDigits(1I))
+        Assert.AreEqual([5], Numbers.GetDigits(5I))
+        Assert.AreEqual([1; 0], Numbers.GetDigits(10I))
+        Assert.AreEqual([1; 1], Numbers.GetDigits(11I))
+        Assert.AreEqual([2; 9], Numbers.GetDigits(29I))
+        Assert.AreEqual([1; 2; 3; 4; 5; 6; 7; 8; 9; 0], Numbers.GetDigits(1234567890I))
+        Assert.AreEqual([2; 1; 4; 7; 4; 8; 3; 6; 4; 7], Numbers.GetDigits(bigint maxint))
+        Assert.AreEqual([9; 2; 2; 3; 3; 7; 2; 0; 3; 6; 8; 5; 4; 7; 7; 5; 8; 0; 7], Numbers.GetDigits(bigint maxint64))
+        Assert.AreEqual([1; 2; 3; 4; 5; 6; 7; 8; 9; 0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 0], Numbers.GetDigits(12345678901234567890I))
+
+    [<Test>]
+    member public this.GetDigitsWithRadix() =
+        // int
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(1, -1) |> ignore) |> ignore
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(1, 0) |> ignore) |> ignore
+        Assert.Throws<NotSupportedException>(fun() -> Numbers.GetDigits(1, 11) |> ignore) |> ignore
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(-1, 10) |> ignore) |> ignore
+        // int64
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(1L, -1) |> ignore) |> ignore
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(1L, 0) |> ignore) |> ignore
+        Assert.Throws<NotSupportedException>(fun() -> Numbers.GetDigits(1L, 11) |> ignore) |> ignore
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(-1L, 10) |> ignore) |> ignore
+        // bigint
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(1I, -1) |> ignore) |> ignore
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(1I, 0) |> ignore) |> ignore
+        Assert.Throws<NotSupportedException>(fun() -> Numbers.GetDigits(1I, 11) |> ignore) |> ignore
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(-1I, 10) |> ignore) |> ignore
+
+    [<Test>]
+    member public this.GetDigitsWithRadix2() =
+        // int
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(-1, 2) |> ignore) |> ignore
+        Assert.AreEqual([0], Numbers.GetDigits(0, 2))
+        Assert.AreEqual([1], Numbers.GetDigits(1, 2))
+        Assert.AreEqual([1; 0], Numbers.GetDigits(2, 2))
+        Assert.AreEqual([1; 1], Numbers.GetDigits(3, 2))
+        Assert.AreEqual([1; 0; 0], Numbers.GetDigits(4, 2))
+        Assert.AreEqual([1; 0; 1], Numbers.GetDigits(5, 2))
+        Assert.AreEqual([1; 1; 0], Numbers.GetDigits(6, 2))
+        Assert.AreEqual([1; 1; 1], Numbers.GetDigits(7, 2))
+        Assert.AreEqual([1; 0; 0; 0], Numbers.GetDigits(8, 2))
+        Assert.AreEqual([1; 0; 0; 1], Numbers.GetDigits(9, 2))
+        Assert.AreEqual([1; 0; 1; 0], Numbers.GetDigits(10, 2))
+        Assert.AreEqual([1; 0; 1; 1], Numbers.GetDigits(11, 2))
+        Assert.AreEqual([1; 1; 0; 0], Numbers.GetDigits(12, 2))
+        Assert.AreEqual([1; 1; 0; 1], Numbers.GetDigits(13, 2))
+        Assert.AreEqual([1; 1; 1; 0], Numbers.GetDigits(14, 2))
+        Assert.AreEqual([1; 1; 1; 1], Numbers.GetDigits(15, 2))
+        // int64
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(-1L, 2) |> ignore) |> ignore
+        Assert.AreEqual([0], Numbers.GetDigits(0L, 2))
+        Assert.AreEqual([1], Numbers.GetDigits(1L, 2))
+        Assert.AreEqual([1; 0], Numbers.GetDigits(2L, 2))
+        Assert.AreEqual([1; 1], Numbers.GetDigits(3L, 2))
+        Assert.AreEqual([1; 0; 0], Numbers.GetDigits(4L, 2))
+        Assert.AreEqual([1; 0; 1], Numbers.GetDigits(5L, 2))
+        Assert.AreEqual([1; 1; 0], Numbers.GetDigits(6L, 2))
+        Assert.AreEqual([1; 1; 1], Numbers.GetDigits(7L, 2))
+        Assert.AreEqual([1; 0; 0; 0], Numbers.GetDigits(8L, 2))
+        Assert.AreEqual([1; 0; 0; 1], Numbers.GetDigits(9L, 2))
+        Assert.AreEqual([1; 0; 1; 0], Numbers.GetDigits(10L, 2))
+        Assert.AreEqual([1; 0; 1; 1], Numbers.GetDigits(11L, 2))
+        Assert.AreEqual([1; 1; 0; 0], Numbers.GetDigits(12L, 2))
+        Assert.AreEqual([1; 1; 0; 1], Numbers.GetDigits(13L, 2))
+        Assert.AreEqual([1; 1; 1; 0], Numbers.GetDigits(14L, 2))
+        Assert.AreEqual([1; 1; 1; 1], Numbers.GetDigits(15L, 2))
+        // bigint
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(-1I, 2) |> ignore) |> ignore
+        Assert.AreEqual([0], Numbers.GetDigits(0I, 2))
+        Assert.AreEqual([1], Numbers.GetDigits(1I, 2))
+        Assert.AreEqual([1; 0], Numbers.GetDigits(2I, 2))
+        Assert.AreEqual([1; 1], Numbers.GetDigits(3I, 2))
+        Assert.AreEqual([1; 0; 0], Numbers.GetDigits(4I, 2))
+        Assert.AreEqual([1; 0; 1], Numbers.GetDigits(5I, 2))
+        Assert.AreEqual([1; 1; 0], Numbers.GetDigits(6I, 2))
+        Assert.AreEqual([1; 1; 1], Numbers.GetDigits(7I, 2))
+        Assert.AreEqual([1; 0; 0; 0], Numbers.GetDigits(8I, 2))
+        Assert.AreEqual([1; 0; 0; 1], Numbers.GetDigits(9I, 2))
+        Assert.AreEqual([1; 0; 1; 0], Numbers.GetDigits(10I, 2))
+        Assert.AreEqual([1; 0; 1; 1], Numbers.GetDigits(11I, 2))
+        Assert.AreEqual([1; 1; 0; 0], Numbers.GetDigits(12I, 2))
+        Assert.AreEqual([1; 1; 0; 1], Numbers.GetDigits(13I, 2))
+        Assert.AreEqual([1; 1; 1; 0], Numbers.GetDigits(14I, 2))
+        Assert.AreEqual([1; 1; 1; 1], Numbers.GetDigits(15I, 2))
+
+    [<Test>]
+    member public this.GetDigitsWithRadix3() =
+        // int
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(-1, 3) |> ignore) |> ignore
+        Assert.AreEqual([0], Numbers.GetDigits(0, 3))
+        Assert.AreEqual([1], Numbers.GetDigits(1, 3))
+        Assert.AreEqual([2], Numbers.GetDigits(2, 3))
+        Assert.AreEqual([1; 0], Numbers.GetDigits(3, 3))
+        Assert.AreEqual([1; 1], Numbers.GetDigits(4, 3))
+        Assert.AreEqual([1; 2], Numbers.GetDigits(5, 3))
+        Assert.AreEqual([2; 0], Numbers.GetDigits(6, 3))
+        Assert.AreEqual([2; 1], Numbers.GetDigits(7, 3))
+        Assert.AreEqual([2; 2], Numbers.GetDigits(8, 3))
+        Assert.AreEqual([1; 0; 0], Numbers.GetDigits(9, 3))
+        Assert.AreEqual([1; 0; 1], Numbers.GetDigits(10, 3))
+        Assert.AreEqual([1; 0; 2], Numbers.GetDigits(11, 3))
+        Assert.AreEqual([1; 1; 0], Numbers.GetDigits(12, 3))
+        Assert.AreEqual([1; 1; 1], Numbers.GetDigits(13, 3))
+        Assert.AreEqual([1; 1; 2], Numbers.GetDigits(14, 3))
+        Assert.AreEqual([1; 2; 0], Numbers.GetDigits(15, 3))
+        Assert.AreEqual([1; 2; 1], Numbers.GetDigits(16, 3))
+        Assert.AreEqual([1; 2; 2], Numbers.GetDigits(17, 3))
+        Assert.AreEqual([2; 0; 0], Numbers.GetDigits(18, 3))
+        Assert.AreEqual([2; 0; 1], Numbers.GetDigits(19, 3))
+        Assert.AreEqual([2; 0; 2], Numbers.GetDigits(20, 3))
+        Assert.AreEqual([2; 1; 0], Numbers.GetDigits(21, 3))
+        Assert.AreEqual([2; 1; 1], Numbers.GetDigits(22, 3))
+        Assert.AreEqual([2; 1; 2], Numbers.GetDigits(23, 3))
+        Assert.AreEqual([2; 2; 0], Numbers.GetDigits(24, 3))
+        Assert.AreEqual([2; 2; 1], Numbers.GetDigits(25, 3))
+        Assert.AreEqual([2; 2; 2], Numbers.GetDigits(26, 3))
+        // int64
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(-1L, 3) |> ignore) |> ignore
+        Assert.AreEqual([0], Numbers.GetDigits(0L, 3))
+        Assert.AreEqual([1], Numbers.GetDigits(1L, 3))
+        Assert.AreEqual([2], Numbers.GetDigits(2L, 3))
+        Assert.AreEqual([1; 0], Numbers.GetDigits(3L, 3))
+        Assert.AreEqual([1; 1], Numbers.GetDigits(4L, 3))
+        Assert.AreEqual([1; 2], Numbers.GetDigits(5L, 3))
+        Assert.AreEqual([2; 0], Numbers.GetDigits(6L, 3))
+        Assert.AreEqual([2; 1], Numbers.GetDigits(7L, 3))
+        Assert.AreEqual([2; 2], Numbers.GetDigits(8L, 3))
+        Assert.AreEqual([1; 0; 0], Numbers.GetDigits(9L, 3))
+        Assert.AreEqual([1; 0; 1], Numbers.GetDigits(10L, 3))
+        Assert.AreEqual([1; 0; 2], Numbers.GetDigits(11L, 3))
+        Assert.AreEqual([1; 1; 0], Numbers.GetDigits(12L, 3))
+        Assert.AreEqual([1; 1; 1], Numbers.GetDigits(13L, 3))
+        Assert.AreEqual([1; 1; 2], Numbers.GetDigits(14L, 3))
+        Assert.AreEqual([1; 2; 0], Numbers.GetDigits(15L, 3))
+        Assert.AreEqual([1; 2; 1], Numbers.GetDigits(16L, 3))
+        Assert.AreEqual([1; 2; 2], Numbers.GetDigits(17L, 3))
+        Assert.AreEqual([2; 0; 0], Numbers.GetDigits(18L, 3))
+        Assert.AreEqual([2; 0; 1], Numbers.GetDigits(19L, 3))
+        Assert.AreEqual([2; 0; 2], Numbers.GetDigits(20L, 3))
+        Assert.AreEqual([2; 1; 0], Numbers.GetDigits(21L, 3))
+        Assert.AreEqual([2; 1; 1], Numbers.GetDigits(22L, 3))
+        Assert.AreEqual([2; 1; 2], Numbers.GetDigits(23L, 3))
+        Assert.AreEqual([2; 2; 0], Numbers.GetDigits(24L, 3))
+        Assert.AreEqual([2; 2; 1], Numbers.GetDigits(25L, 3))
+        Assert.AreEqual([2; 2; 2], Numbers.GetDigits(26L, 3))
+        // bigint
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetDigits(-1I, 3) |> ignore) |> ignore
+        Assert.AreEqual([0], Numbers.GetDigits(0I, 3))
+        Assert.AreEqual([1], Numbers.GetDigits(1I, 3))
+        Assert.AreEqual([2], Numbers.GetDigits(2I, 3))
+        Assert.AreEqual([1; 0], Numbers.GetDigits(3I, 3))
+        Assert.AreEqual([1; 1], Numbers.GetDigits(4I, 3))
+        Assert.AreEqual([1; 2], Numbers.GetDigits(5I, 3))
+        Assert.AreEqual([2; 0], Numbers.GetDigits(6I, 3))
+        Assert.AreEqual([2; 1], Numbers.GetDigits(7I, 3))
+        Assert.AreEqual([2; 2], Numbers.GetDigits(8I, 3))
+        Assert.AreEqual([1; 0; 0], Numbers.GetDigits(9I, 3))
+        Assert.AreEqual([1; 0; 1], Numbers.GetDigits(10I, 3))
+        Assert.AreEqual([1; 0; 2], Numbers.GetDigits(11I, 3))
+        Assert.AreEqual([1; 1; 0], Numbers.GetDigits(12I, 3))
+        Assert.AreEqual([1; 1; 1], Numbers.GetDigits(13I, 3))
+        Assert.AreEqual([1; 1; 2], Numbers.GetDigits(14I, 3))
+        Assert.AreEqual([1; 2; 0], Numbers.GetDigits(15I, 3))
+        Assert.AreEqual([1; 2; 1], Numbers.GetDigits(16I, 3))
+        Assert.AreEqual([1; 2; 2], Numbers.GetDigits(17I, 3))
+        Assert.AreEqual([2; 0; 0], Numbers.GetDigits(18I, 3))
+        Assert.AreEqual([2; 0; 1], Numbers.GetDigits(19I, 3))
+        Assert.AreEqual([2; 0; 2], Numbers.GetDigits(20I, 3))
+        Assert.AreEqual([2; 1; 0], Numbers.GetDigits(21I, 3))
+        Assert.AreEqual([2; 1; 1], Numbers.GetDigits(22I, 3))
+        Assert.AreEqual([2; 1; 2], Numbers.GetDigits(23I, 3))
+        Assert.AreEqual([2; 2; 0], Numbers.GetDigits(24I, 3))
+        Assert.AreEqual([2; 2; 1], Numbers.GetDigits(25I, 3))
+        Assert.AreEqual([2; 2; 2], Numbers.GetDigits(26I, 3))
+
+    [<Test>]
+    member public this.GetNumber() =
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetNumber([0; 1; -1]) |> ignore) |> ignore
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetNumber([0; 1; 11]) |> ignore) |> ignore
+        Assert.AreEqual(0I, Numbers.GetNumber([0]))
+        Assert.AreEqual(1I, Numbers.GetNumber([1]))
+        Assert.AreEqual(5I, Numbers.GetNumber([5]))
+        Assert.AreEqual(10I, Numbers.GetNumber([1; 0]))
+        Assert.AreEqual(11I, Numbers.GetNumber([1; 1]))
+        Assert.AreEqual(29I, Numbers.GetNumber([2; 9]))
+        Assert.AreEqual(1234567890I, Numbers.GetNumber([1; 2; 3; 4; 5; 6; 7; 8; 9; 0]))
+        Assert.AreEqual(bigint maxint, Numbers.GetNumber([2; 1; 4; 7; 4; 8; 3; 6; 4; 7]))
+        Assert.AreEqual(bigint maxint64, Numbers.GetNumber([9; 2; 2; 3; 3; 7; 2; 0; 3; 6; 8; 5; 4; 7; 7; 5; 8; 0; 7]))
+        Assert.AreEqual(12345678901234567890I, Numbers.GetNumber([1; 2; 3; 4; 5; 6; 7; 8; 9; 0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 0]))
+
+    [<Test>]
+    member public this.GetNumberWithRadix() =
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetNumber([0; 1; -1], 2) |> ignore) |> ignore
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetNumber([0; 1; 3], 2) |> ignore) |> ignore
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetNumber([0; 1; 2], -1) |> ignore) |> ignore
+        Assert.Throws<NotSupportedException>(fun() -> Numbers.GetNumber([0; 1; 2], 11) |> ignore) |> ignore
+
+    [<Test>]
+    member public this.GetNumberWithRadix2() =
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetNumber([0; 1; -1], 2) |> ignore) |> ignore
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetNumber([0; 1; 2], 2) |> ignore) |> ignore
+        Assert.AreEqual(0I, Numbers.GetNumber([0], 2))
+        Assert.AreEqual(1I, Numbers.GetNumber([1], 2))
+        Assert.AreEqual(2I, Numbers.GetNumber([1; 0], 2))
+        Assert.AreEqual(3I, Numbers.GetNumber([1; 1], 2))
+        Assert.AreEqual(4I, Numbers.GetNumber([1; 0; 0], 2))
+        Assert.AreEqual(5I, Numbers.GetNumber([1; 0; 1], 2))
+        Assert.AreEqual(6I, Numbers.GetNumber([1; 1; 0], 2))
+        Assert.AreEqual(7I, Numbers.GetNumber([1; 1; 1], 2))
+        Assert.AreEqual(8I, Numbers.GetNumber([1; 0; 0; 0], 2))
+        Assert.AreEqual(9I, Numbers.GetNumber([1; 0; 0; 1], 2))
+        Assert.AreEqual(10I, Numbers.GetNumber([1; 0; 1; 0], 2))
+        Assert.AreEqual(11I, Numbers.GetNumber([1; 0; 1; 1], 2))
+        Assert.AreEqual(12I, Numbers.GetNumber([1; 1; 0; 0], 2))
+        Assert.AreEqual(13I, Numbers.GetNumber([1; 1; 0; 1], 2))
+        Assert.AreEqual(14I, Numbers.GetNumber([1; 1; 1; 0], 2))
+        Assert.AreEqual(15I, Numbers.GetNumber([1; 1; 1; 1], 2))
+
+    [<Test>]
+    member public this.GetNumberWithRadix3() =
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetNumber([0; 1; -1], 3) |> ignore) |> ignore
+        Assert.Throws<ArgumentOutOfRangeException>(fun() -> Numbers.GetNumber([0; 1; 3], 3) |> ignore) |> ignore
+        Assert.AreEqual(0I, Numbers.GetNumber([0], 3))
+        Assert.AreEqual(1I, Numbers.GetNumber([1], 3))
+        Assert.AreEqual(2I, Numbers.GetNumber([2], 3))
+        Assert.AreEqual(3I, Numbers.GetNumber([1; 0], 3))
+        Assert.AreEqual(4I, Numbers.GetNumber([1; 1], 3))
+        Assert.AreEqual(5I, Numbers.GetNumber([1; 2], 3))
+        Assert.AreEqual(6I, Numbers.GetNumber([2; 0], 3))
+        Assert.AreEqual(7I, Numbers.GetNumber([2; 1], 3))
+        Assert.AreEqual(8I, Numbers.GetNumber([2; 2], 3))
+        Assert.AreEqual(9I, Numbers.GetNumber([1; 0; 0], 3))
+        Assert.AreEqual(10I, Numbers.GetNumber([1; 0; 1], 3))
+        Assert.AreEqual(11I, Numbers.GetNumber([1; 0; 2], 3))
+        Assert.AreEqual(12I, Numbers.GetNumber([1; 1; 0], 3))
+        Assert.AreEqual(13I, Numbers.GetNumber([1; 1; 1], 3))
+        Assert.AreEqual(14I, Numbers.GetNumber([1; 1; 2], 3))
+        Assert.AreEqual(15I, Numbers.GetNumber([1; 2; 0], 3))
+        Assert.AreEqual(16I, Numbers.GetNumber([1; 2; 1], 3))
+        Assert.AreEqual(17I, Numbers.GetNumber([1; 2; 2], 3))
+        Assert.AreEqual(18I, Numbers.GetNumber([2; 0; 0], 3))
+        Assert.AreEqual(19I, Numbers.GetNumber([2; 0; 1], 3))
+        Assert.AreEqual(20I, Numbers.GetNumber([2; 0; 2], 3))
+        Assert.AreEqual(21I, Numbers.GetNumber([2; 1; 0], 3))
+        Assert.AreEqual(22I, Numbers.GetNumber([2; 1; 1], 3))
+        Assert.AreEqual(23I, Numbers.GetNumber([2; 1; 2], 3))
+        Assert.AreEqual(24I, Numbers.GetNumber([2; 2; 0], 3))
+        Assert.AreEqual(25I, Numbers.GetNumber([2; 2; 1], 3))
+        Assert.AreEqual(26I, Numbers.GetNumber([2; 2; 2], 3))
