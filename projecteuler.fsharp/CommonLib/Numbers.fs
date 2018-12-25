141afdd695da22383ff47126ad033e62134d9d1d
stdstring <std_string@mail.ru>
2018-12-25 07:26:11 +0500

    reimplemented solution of problem 015

--- projecteuler.fsharp/CommonLib/Numbers.fs
+++ projecteuler.fsharp/CommonLib/Numbers.fs
@@ -53,6 +53,19 @@ type Numbers =
         | _ when number = 0I -> 1I
         | _ -> seq {1I .. number} |> Seq.fold (fun product number -> product * number) 1I
 
+    static member public CalcBinomialCoeff(n: int, k: int) =
+        Numbers.CalcBinomialCoeff(n |> bigint, k |> bigint)
+
+    static member public CalcBinomialCoeff(n: int64, k: int64) =
+        Numbers.CalcBinomialCoeff(n |> bigint, k |> bigint)
+
+    static member public CalcBinomialCoeff(n: bigint, k: bigint) =
+        match n, k with
+        | _, _ when n < 0I -> raise (ArgumentOutOfRangeException("n"))
+        | _, _ when k < 0I -> 0I
+        | _, _ when k > n -> 0I
+        | _, _ -> (seq{n - k + 1I .. n} |> Seq.fold (fun product value -> product * value) 1I) / Numbers.CalcFactorial(k)
+
     static member private CheckRadix(radix: int) =
         match radix with
         | _ when radix < 2 -> raise (ArgumentOutOfRangeException("radix"))
---
+++ projecteuler.fsharp/ProjectEulerTasks/Problem015.fs
@@ -0,0 +1,24 @@
+﻿namespace ProjectEulerTasks
+
+open NUnit.Framework
+open ProjectEulerTasks.Utils
+open CommonLib
+
+// Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+// How many such routes are there through a 20×20 grid?
+
+[<TestFixture>]
+type Problem015() =
+
+    let solveImpl (gridSize: int) =
+        // Solution for N * N grid:
+        // 1) each path contains N horizontal and N vertical regions
+        // 2) It is enough N horizontal regions (or N horizontal regions) for path definition
+        // So for solution we may calculate number of combinations by N from 2 * N (calculate binomial coefficient)
+        Numbers.CalcBinomialCoeff(2 * gridSize, gridSize) |> int64
+
+    [<TestCase(2, 6L, TimeThresholds.HardTimeLimit)>]
+    [<TestCase(3, 20L, TimeThresholds.HardTimeLimit)>]
+    [<TestCase(20, 137846528820L, TimeThresholds.HardTimeLimit)>]
+    member public this.Solve(gridSize: int, expectedAnswer: int64, timeLimit: int) =
+        SolutionUtils.CheckSolution(timeLimit, expectedAnswer, fun() -> solveImpl gridSize)
