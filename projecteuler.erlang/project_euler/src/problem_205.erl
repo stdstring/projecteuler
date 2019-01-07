af4715aa67766ec075c3ff86b8fb17e2869cab85
stdstring <std_string@mail.ru>
2019-01-07 12:00:43 +0500

    reimplemented solution of problem 205 + simplified Erlang implementation

--- projecteuler.erlang/project_euler/src/problem_205.erl
+++ projecteuler.erlang/project_euler/src/problem_205.erl
@@ -13,8 +13,8 @@
 -type dice() :: numbers:digits().
 -type dice_array() :: array:array(numbers:digit()).
 -type dice_roll() :: pos_integer().
--type dices_roll_state() :: {DicesRollSum :: pos_integer(), DicesRoll :: [dice_roll()]}.
--type dices_roll_data() :: dict:dict(DicesRollSum :: pos_integer(), SortedStateIndex :: non_neg_integer()).
+-type dices_roll_state() :: pos_integer().
+-type dices_roll_data() :: array:array(pos_integer()).
 
 %% ====================================================================
 %% API functions
@@ -43,40 +43,42 @@ solve({FirstDice, FirstCount, SecondDice, SecondCount}) ->
 %% Let we have a set of N identical dice (with K faces), then State - the result of some roll of all these dices.
 %% Let L - the set of all possible states for a set of N identical dice.
 %% For each element (state) of L, we calculate the sum of the values of the roll of all dice and
-%% sort the states in the set L according to the value of the sum in ascending order - get an ordered set L '.
+%% sort the states in the set L according to the value of the sum in ascending order - get an ordered set L'.
 %% It is easy to show that the values of sum for all states are in the range [N, N * K] without any holes.
-%% Then, for any value, we can easily find out how many states have a sum less than this value (using an ordered set of L ').
+%% Then, for any value, we can easily find out how many states have a sum less than this value (using an ordered set of L').
 
 -spec process(FirstDice :: dice(), FirstDiceCount :: pos_integer(), SecondDice :: dice(), SecondDiceCount :: pos_integer()) -> non_neg_integer().
 process(FirstDice, FirstDiceCount, SecondDice, SecondDiceCount) ->
-    SecondStates = generate_roll_states(array:from_list(SecondDice), SecondDiceCount),
-    SecondData = prepare_roll_data(SecondStates),
+    SecondDiceArray = array:from_list(SecondDice),
+    SecondStates = generate_roll_states(SecondDiceArray, SecondDiceCount),
+    SecondData = prepare_roll_data(SecondDiceArray, SecondDiceCount, SecondStates),
     FirstStateCount = numbers:power(length(FirstDice), FirstDiceCount),
-    process_impl(array:from_list(FirstDice), FirstDiceCount, 0, FirstStateCount, SecondData, 0).
+    MinValue = array:get(0, SecondDiceArray) * SecondDiceCount,
+    process_impl(array:from_list(FirstDice), FirstDiceCount, 0, FirstStateCount, MinValue, SecondData, 0).
 
 
 -spec process_impl(FirstDice :: dice_array(),
                    FirstDiceCount :: pos_integer(),
                    FirstStateIndex :: non_neg_integer(),
                    FirstStateCount :: pos_integer(),
+                   MinValue :: pos_integer(),
                    SecondData :: dices_roll_data(),
                    Result :: non_neg_integer()) -> non_neg_integer().
-process_impl(_FirstDice, _FirstDiceCount, FirstStateIndex, FirstStateCount, _SecondData, Result) when FirstStateIndex == FirstStateCount -> Result;
-process_impl(FirstDice, FirstDiceCount, FirstStateIndex, FirstStateCount, SecondData, Result) ->
-    {FirstSum, _FirstState} = generate_roll_state(FirstDice, FirstDiceCount, FirstStateIndex),
-    SecondIndex = dict:fetch(FirstSum, SecondData),
-    process_impl(FirstDice, FirstDiceCount, FirstStateIndex + 1, FirstStateCount, SecondData, Result + SecondIndex).
+process_impl(_FirstDice, _FirstDiceCount, FirstStateIndex, FirstStateCount, _MinValue, _SecondData, Result) when FirstStateIndex == FirstStateCount -> Result;
+process_impl(FirstDice, FirstDiceCount, FirstStateIndex, FirstStateCount, MinValue, SecondData, Result) ->
+    FirstSum = generate_roll_state(FirstDice, FirstDiceCount, FirstStateIndex),
+    if
+        FirstSum > MinValue -> process_impl(FirstDice, FirstDiceCount, FirstStateIndex + 1, FirstStateCount, MinValue, SecondData, Result + array:get(FirstSum - MinValue - 1, SecondData));
+        true -> process_impl(FirstDice, FirstDiceCount, FirstStateIndex + 1, FirstStateCount, MinValue, SecondData, Result)
+    end.
 
--spec prepare_roll_data(States :: [dices_roll_state()]) -> dices_roll_data().
-prepare_roll_data(States) ->
-    FoldFun = fun({Sum, _State}, {Data, Index}) ->
-        case dict:is_key(Sum, Data) of
-            true -> {Data, Index + 1};
-            false -> {dict:store(Sum, Index, Data), Index + 1}
-        end
-    end,
-    {DestData, _DestIndex} = lists:foldl(FoldFun, {dict:new(), 0}, lists:sort(States)),
-    DestData.
+-spec prepare_roll_data(Dice :: dice_array(), DiceCount :: pos_integer(), States :: [dices_roll_state()]) -> dices_roll_data().
+prepare_roll_data(Dice, DiceCount, States) ->
+    MinValue = array:get(0, Dice) * DiceCount,
+    MaxValue = array:get(array:size(Dice) - 1, Dice) * DiceCount,
+    FoldFun = fun(Sum, Dest) -> array:set(Sum - MinValue, array:get(Sum - MinValue, Dest) + 1, Dest) end,
+    Storage = lists:foldl(FoldFun, array:new([{size, MaxValue - MinValue + 1}, {fixed, true}, {default, 0}]), States),
+    lists:foldl(fun(Index, Dest) -> array:set(Index, array:get(Index, Dest) + array:get(Index - 1, Dest), Dest) end, Storage, lists:seq(1, array:size(Storage) - 1)).
 
 -spec generate_roll_states(Dice :: dice_array(), DiceCount :: pos_integer()) -> [dices_roll_state()].
 generate_roll_states(Dice, DiceCount) ->
@@ -91,7 +93,7 @@ generate_roll_state(Dice, DiceCount, StateIndex) -> generate_roll_state_impl(Dic
                                DiceCount :: pos_integer(),
                                StateIndex :: non_neg_integer(),
                                State :: [dice_roll()]) -> dices_roll_state().
-generate_roll_state_impl(_Dice, DiceIndex, DiceCount, _StateIndex, State) when DiceIndex == DiceCount -> {lists:sum(State), State};
+generate_roll_state_impl(_Dice, DiceIndex, DiceCount, _StateIndex, State) when DiceIndex == DiceCount -> lists:sum(State);
 generate_roll_state_impl(Dice, DiceIndex, DiceCount, StateIndex, State) ->
     NumberIndex = StateIndex rem array:size(Dice),
     Number = array:get(NumberIndex, Dice),
