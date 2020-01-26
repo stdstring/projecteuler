aab3deec8a7934778c3a215192511259a5706f90
stdstring <std_string@mail.ru>
2020-01-26 23:48:55 +0500

    added performance tests on EratosSieveWithSmallestPrimeFactors & EulerTotientFunction

--- projecteuler.fsharp/CommonLibPerformanceTests/EratosSieveTests.fs
+++ projecteuler.fsharp/CommonLibPerformanceTests/EratosSieveTests.fs
@@ -19,3 +19,19 @@ type EratosSieveTests() =
     [<TestCase(1000000000)>]
     member public this.CreateSieve(maxNumber: int) =
         (fun () -> EratosSieve.Create(maxNumber) |> ignore) |> MeasureUtils.MeasureExecutionTime |> ignore
+
+[<TestFixture>]
+[<Category("Performance")>]
+type EratosSieveWithSmallestPrimeFactorsTests() =
+
+    [<TestCase(10)>]
+    [<TestCase(100)>]
+    [<TestCase(1000)>]
+    [<TestCase(10000)>]
+    [<TestCase(100000)>]
+    [<TestCase(1000000)>]
+    [<TestCase(10000000)>]
+    [<TestCase(100000000)>]
+    [<TestCase(500000000)>]
+    member public this.CreateSieve(maxNumber: int) =
+        (fun () -> EratosSieveWithSmallestPrimeFactors.Create(maxNumber) |> ignore) |> MeasureUtils.MeasureExecutionTime |> ignore
---
+++ projecteuler.fsharp/CommonLibPerformanceTests/EulerTotientFunctionTests.fs
@@ -0,0 +1,21 @@
+﻿namespace CommonLibPerformanceTests
+
+open CommonLib
+open NUnit.Framework
+open CommonLibPerformanceTests.Utils
+
+[<TestFixture>]
+[<Category("Performance")>]
+type EulerTotientFunctionTests() =
+
+    [<TestCase(10)>]
+    [<TestCase(100)>]
+    [<TestCase(1000)>]
+    [<TestCase(10000)>]
+    [<TestCase(100000)>]
+    [<TestCase(1000000)>]
+    [<TestCase(10000000)>]
+    [<TestCase(100000000)>]
+    [<TestCase(250000000)>]
+    member public this.CreateSieve(maxNumber: int) =
+        (fun () -> EulerTotientFunction.Create(maxNumber) |> ignore) |> MeasureUtils.MeasureExecutionTime |> ignore
