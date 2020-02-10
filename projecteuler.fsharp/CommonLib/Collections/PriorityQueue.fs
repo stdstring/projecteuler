c087662df3faf43afb955abac5c4aad34643a363
stdstring <std_string@mail.ru>
2020-02-10 08:29:32 +0500

    implemented solution of problem 107

---
+++ projecteuler.fsharp/CommonLib/Collections/PriorityQueue.fs
@@ -0,0 +1,105 @@
+﻿namespace CommonLib.Collections
+
+open System
+
+// TODO (std_string) : think about moving binary heap into separate data type
+type PriorityQueue<'TItem> public(less: 'TItem * 'TItem -> bool) =
+
+    let binaryHeap = ResizeArray<'TItem>()
+    do
+        // first entry isn't used
+        Unchecked.defaultof<'TItem> |> binaryHeap.Add
+
+    member public this.Insert(item: 'TItem) =
+        item |> binaryHeap.Add
+        binaryHeap.Count - 1 |> this.Swim
+
+    member public this.GetTop =
+        if binaryHeap.Count = 1 then
+            raise (InvalidOperationException())
+        binaryHeap.[1]
+
+    member public this.ExtraxtTop() =
+        if binaryHeap.Count = 1 then
+            raise (InvalidOperationException())
+        let top = binaryHeap.[1]
+        binaryHeap.[1] <- binaryHeap.[binaryHeap.Count - 1]
+        binaryHeap.RemoveAt(binaryHeap.Count - 1)
+        1 |> this.Sink
+        top
+
+    member public this.IsEmpty = binaryHeap.Count = 1
+
+    // binary heap interface
+
+    // get parent index
+    member private this.GetParent(current: int) = current / 2
+
+    // get left child index
+    member private this.GetLeftChild(current: int) = 2 * current
+
+    // get right child index
+    member private this.GetRightChild(current: int) = 2 * current + 1
+
+    // is index valid
+    member private this.IsValid(current: int) = (current > 0) && (current < binaryHeap.Count - 1)
+
+    // from https://algs4.cs.princeton.edu/24pq/
+    member private this.Swim(current: int) =
+        let parent = current |> this.GetParent
+        match parent with
+        | _ when parent |> this.IsValid |> not -> ()
+        | _ when less(binaryHeap.[parent], binaryHeap.[current]) |> not -> ()
+        | _ ->
+            this.Exchange(parent, current)
+            parent |> this.Swim
+
+    // from https://algs4.cs.princeton.edu/24pq/
+    member private this.Sink(current: int) =
+        let leftChild = current |> this.GetLeftChild
+        let rightChild = leftChild + 1
+        match leftChild with
+        | _ when leftChild |> this.IsValid |> not -> ()
+        | _ when (rightChild |> this.IsValid) && less(binaryHeap.[leftChild], binaryHeap.[rightChild]) ->
+            match less(binaryHeap.[current], binaryHeap.[rightChild]) with
+            | false -> ()
+            | true ->
+                this.Exchange(current, rightChild)
+                rightChild |> this.Sink
+        | _ ->
+            match less(binaryHeap.[current], binaryHeap.[leftChild]) with
+            | false -> ()
+            | true ->
+                this.Exchange(current, leftChild)
+                leftChild |> this.Sink
+
+    member private this.Exchange(left: int, right: int) =
+        let value = binaryHeap.[left]
+        binaryHeap.[left] <- binaryHeap.[right]
+        binaryHeap.[right] <- value
+
+type MaxPriorityQueue<'TItem> public(compare: 'TItem * 'TItem -> int) =
+
+    let priorityQueue = PriorityQueue(fun (left, right) -> compare(left, right) < 0)
+
+    member public this.Insert(item: 'TItem) =
+        item |> priorityQueue.Insert
+
+    member public this.GetMax = priorityQueue.GetTop
+
+    member public this.ExtractMax() = priorityQueue.ExtraxtTop()
+
+    member public this.IsEmpty = priorityQueue.IsEmpty
+
+type MinPriorityQueue<'TItem> public(compare: 'TItem * 'TItem -> int) =
+
+    let priorityQueue = PriorityQueue(fun (left, right) -> compare(left, right) > 0)
+
+    member public this.Insert(item: 'TItem) =
+        item |> priorityQueue.Insert
+
+    member public this.GetMin = priorityQueue.GetTop
+
+    member public this.ExtractMin() = priorityQueue.ExtraxtTop()
+
+    member public this.IsEmpty = priorityQueue.IsEmpty
---
+++ projecteuler.fsharp/CommonLibTests/Collections/PriorityQueueTests.fs
@@ -0,0 +1,184 @@
+﻿namespace CommonLibTests.Collections
+
+open CommonLib.Collections
+open NUnit.Framework
+open System
+
+module PriorityQueueTestsImpl =
+    type SomeData = {FieldA: int; FieldB: string}
+
+    let creareSomeData (valueA: int) (valueB: string) = {SomeData.FieldA = valueA; SomeData.FieldB = valueB}
+
+open PriorityQueueTestsImpl
+
+[<TestFixture>]
+type MaxPriorityQueueTests() =
+
+    let insertItems(queue: MaxPriorityQueue<'TItem>) (items: 'TItem[]) =
+        items |> Seq.iter (fun item -> item |> queue.Insert)
+
+    let insertItemsWithCheckMax(queue: MaxPriorityQueue<'TItem>) (itemAndMaxPairs: ('TItem * 'TItem)[]) =
+        for (item, expectedMax) in itemAndMaxPairs do
+            item |> queue.Insert
+            Assert.AreEqual(expectedMax, queue.GetMax)
+
+    let checkExtractMax (queue: MaxPriorityQueue<'TItem>) (expectedMax: 'TItem[]) =
+        for max in expectedMax do
+            Assert.AreEqual(max, queue.ExtractMax())
+        queue.IsEmpty |> Assert.IsTrue
+
+    [<Test>]
+    member public this.Insert() =
+        // max priority queue for int
+        let intQueue = MaxPriorityQueue<int>(fun (left, right) -> left - right)
+        intQueue.IsEmpty |> Assert.IsTrue
+        [|(1, 1); (4, 4); (3, 4); (13, 13); (11, 13)|] |> insertItemsWithCheckMax intQueue
+        intQueue.ExtractMax() |> ignore
+        666 |> intQueue.Insert
+        Assert.AreEqual(666, intQueue.GetMax)
+        // max priority queue for SomeData
+        let someDataQueue = MaxPriorityQueue<SomeData>(fun (left, right) -> left.FieldA - right.FieldA)
+        someDataQueue.IsEmpty |> Assert.IsTrue
+        [|(creareSomeData 1 "IDDQD", creareSomeData 1 "IDDQD");
+          (creareSomeData 4 "IDKFA", creareSomeData 4 "IDKFA");
+          (creareSomeData 3 "IDCLIP", creareSomeData 4 "IDKFA");
+          (creareSomeData 13 "IMPULSE 9", creareSomeData 13 "IMPULSE 9");
+          (creareSomeData 11 "IMPULSE 666", creareSomeData 13 "IMPULSE 9")|] |> insertItemsWithCheckMax someDataQueue
+        someDataQueue.ExtractMax() |> ignore
+        creareSomeData 666 "666" |> someDataQueue.Insert
+        Assert.AreEqual(creareSomeData 666 "666", someDataQueue.GetMax)
+
+    [<Test>]
+    member public this.GetMax() =
+        // max priority queue for int
+        let intQueue = MaxPriorityQueue<int>(fun (left, right) -> left - right)
+        Assert.Throws<InvalidOperationException>(fun() -> intQueue.GetMax |> ignore) |> ignore
+        [|(1, 1); (4, 4); (3, 4); (13, 13); (11, 13)|] |> insertItemsWithCheckMax intQueue
+        // max priority queue for SomeData
+        let someDataQueue = MaxPriorityQueue<SomeData>(fun (left, right) -> left.FieldA - right.FieldA)
+        Assert.Throws<InvalidOperationException>(fun() -> someDataQueue.GetMax |> ignore) |> ignore
+        [|(creareSomeData 1 "IDDQD", creareSomeData 1 "IDDQD");
+          (creareSomeData 4 "IDKFA", creareSomeData 4 "IDKFA");
+          (creareSomeData 3 "IDCLIP", creareSomeData 4 "IDKFA");
+          (creareSomeData 13 "IMPULSE 9", creareSomeData 13 "IMPULSE 9");
+          (creareSomeData 11 "IMPULSE 666", creareSomeData 13 "IMPULSE 9")|] |> insertItemsWithCheckMax someDataQueue
+
+    [<Test>]
+    member public this.ExtractMax() =
+        // max priority queue for int
+        let intQueue = MaxPriorityQueue<int>(fun (left, right) -> left - right)
+        Assert.Throws<InvalidOperationException>(fun() -> intQueue.ExtractMax() |> ignore) |> ignore
+        [|1; 4; 3; 13; 11|] |> insertItems intQueue
+        [|13; 11; 4; 3; 1|] |> checkExtractMax intQueue
+        // max priority queue for SomeData
+        let someDataQueue = MaxPriorityQueue<SomeData>(fun (left, right) -> left.FieldA - right.FieldA)
+        Assert.Throws<InvalidOperationException>(fun() -> someDataQueue.ExtractMax() |> ignore) |> ignore
+        [|creareSomeData 1 "IDDQD"; creareSomeData 4 "IDKFA"; creareSomeData 3 "IDCLIP"; creareSomeData 13 "IMPULSE 9"; creareSomeData 11 "IMPULSE 666"|] |> insertItems someDataQueue
+        [|creareSomeData 13 "IMPULSE 9"; creareSomeData 11 "IMPULSE 666"; creareSomeData 4 "IDKFA"; creareSomeData 3 "IDCLIP"; creareSomeData 1 "IDDQD"|] |> checkExtractMax someDataQueue
+
+    [<Test>]
+    member public this.IsEmpty() =
+        // max priority queue for int
+        let intQueue = MaxPriorityQueue<int>(fun (left, right) -> left - right)
+        intQueue.IsEmpty |> Assert.IsTrue
+        Assert.DoesNotThrow(fun() -> 666 |> intQueue.Insert)
+        intQueue.IsEmpty |> Assert.IsFalse
+        Assert.DoesNotThrow(fun() -> intQueue.GetMax |> ignore)
+        intQueue.IsEmpty |> Assert.IsFalse
+        Assert.DoesNotThrow(fun() -> intQueue.ExtractMax() |> ignore)
+        intQueue.IsEmpty |> Assert.IsTrue
+        // max priority queue for SomeData
+        let someDataQueue = MaxPriorityQueue<SomeData>(fun (left, right) -> left.FieldA - right.FieldA)
+        someDataQueue.IsEmpty |> Assert.IsTrue
+        Assert.DoesNotThrow(fun() -> creareSomeData 666 "IDDQD" |> someDataQueue.Insert)
+        someDataQueue.IsEmpty |> Assert.IsFalse
+        Assert.DoesNotThrow(fun() -> someDataQueue.GetMax |> ignore)
+        someDataQueue.IsEmpty |> Assert.IsFalse
+        Assert.DoesNotThrow(fun() -> someDataQueue.ExtractMax() |> ignore)
+        someDataQueue.IsEmpty |> Assert.IsTrue
+
+[<TestFixture>]
+type MinPriorityQueueTests() =
+
+    let insertItems(queue: MinPriorityQueue<'TItem>) (items: 'TItem[]) =
+        items |> Seq.iter (fun item -> item |> queue.Insert)
+
+    let insertItemsWithCheckMin(queue: MinPriorityQueue<'TItem>) (itemAndMinPairs: ('TItem * 'TItem)[]) =
+        for (item, expectedMin) in itemAndMinPairs do
+            item |> queue.Insert
+            Assert.AreEqual(expectedMin, queue.GetMin)
+
+    let checkExtractMin (queue: MinPriorityQueue<'TItem>) (expectedMin: 'TItem[]) =
+        for min in expectedMin do
+            Assert.AreEqual(min, queue.ExtractMin())
+        queue.IsEmpty |> Assert.IsTrue
+
+    [<Test>]
+    member public this.Insert() =
+        // min priority queue for int
+        let intQueue = MinPriorityQueue<int>(fun (left, right) -> left - right)
+        intQueue.IsEmpty |> Assert.IsTrue
+        [|(11, 11); (13, 11); (3, 3); (4, 3); (1, 1)|] |> insertItemsWithCheckMin intQueue
+        intQueue.ExtractMin() |> ignore
+        -19 |> intQueue.Insert
+        Assert.AreEqual(-19, intQueue.GetMin)
+        // min priority queue for SomeData
+        let someDataQueue = MinPriorityQueue<SomeData>(fun (left, right) -> left.FieldA - right.FieldA)
+        someDataQueue.IsEmpty |> Assert.IsTrue
+        [|(creareSomeData 11 "IDDQD", creareSomeData 11 "IDDQD");
+          (creareSomeData 13 "IDKFA", creareSomeData 11 "IDDQD");
+          (creareSomeData 3 "IDCLIP", creareSomeData 3 "IDCLIP");
+          (creareSomeData 4 "IMPULSE 9", creareSomeData 3 "IDCLIP");
+          (creareSomeData 1 "IMPULSE 666", creareSomeData 1 "IMPULSE 666")|] |> insertItemsWithCheckMin someDataQueue
+        someDataQueue.ExtractMin() |> ignore
+        creareSomeData -19 "-19" |> someDataQueue.Insert
+        Assert.AreEqual(creareSomeData -19 "-19", someDataQueue.GetMin)
+
+    [<Test>]
+    member public this.GetMin() =
+        // min priority queue for int
+        let intQueue = MinPriorityQueue<int>(fun (left, right) -> left - right)
+        Assert.Throws<InvalidOperationException>(fun() -> intQueue.GetMin |> ignore) |> ignore
+        [|(11, 11); (13, 11); (3, 3); (4, 3); (1, 1)|] |> insertItemsWithCheckMin intQueue
+        // min priority queue for SomeData
+        let someDataQueue = MinPriorityQueue<SomeData>(fun (left, right) -> left.FieldA - right.FieldA)
+        Assert.Throws<InvalidOperationException>(fun() -> someDataQueue.GetMin |> ignore) |> ignore
+        [|(creareSomeData 11 "IDDQD", creareSomeData 11 "IDDQD");
+          (creareSomeData 13 "IDKFA", creareSomeData 11 "IDDQD");
+          (creareSomeData 3 "IDCLIP", creareSomeData 3 "IDCLIP");
+          (creareSomeData 4 "IMPULSE 9", creareSomeData 3 "IDCLIP");
+          (creareSomeData 1 "IMPULSE 666", creareSomeData 1 "IMPULSE 666")|] |> insertItemsWithCheckMin someDataQueue
+
+    [<Test>]
+    member public this.ExtractMin() =
+        // min priority queue for int
+        let intQueue = MinPriorityQueue<int>(fun (left, right) -> left - right)
+        Assert.Throws<InvalidOperationException>(fun() -> intQueue.ExtractMin() |> ignore) |> ignore
+        [|13; 4; 11; 1; 3|] |> insertItems intQueue
+        [|1; 3; 4; 11; 13|] |> checkExtractMin intQueue
+        // min priority queue for SomeData
+        let someDataQueue = MinPriorityQueue<SomeData>(fun (left, right) -> left.FieldA - right.FieldA)
+        Assert.Throws<InvalidOperationException>(fun() -> someDataQueue.ExtractMin() |> ignore) |> ignore
+        [|creareSomeData 13 "IDDQD"; creareSomeData 4 "IDKFA"; creareSomeData 11 "IDCLIP"; creareSomeData 1 "IMPULSE 9"; creareSomeData 3 "IMPULSE 666"|] |> insertItems someDataQueue
+        [|creareSomeData 1 "IMPULSE 9"; creareSomeData 3 "IMPULSE 666"; creareSomeData 4 "IDKFA"; creareSomeData 11 "IDCLIP"; creareSomeData 13 "IDDQD"|] |> checkExtractMin someDataQueue
+
+    [<Test>]
+    member public this.IsEmpty() =
+        // min priority queue for int
+        let intQueue = MinPriorityQueue<int>(fun (left, right) -> left - right)
+        intQueue.IsEmpty |> Assert.IsTrue
+        Assert.DoesNotThrow(fun() -> 666 |> intQueue.Insert)
+        intQueue.IsEmpty |> Assert.IsFalse
+        Assert.DoesNotThrow(fun() -> intQueue.GetMin |> ignore)
+        intQueue.IsEmpty |> Assert.IsFalse
+        Assert.DoesNotThrow(fun() -> intQueue.ExtractMin() |> ignore)
+        intQueue.IsEmpty |> Assert.IsTrue
+        // min priority queue for SomeData
+        let someDataQueue = MinPriorityQueue<SomeData>(fun (left, right) -> left.FieldA - right.FieldA)
+        someDataQueue.IsEmpty |> Assert.IsTrue
+        Assert.DoesNotThrow(fun() -> creareSomeData 666 "IDDQD" |> someDataQueue.Insert)
+        someDataQueue.IsEmpty |> Assert.IsFalse
+        Assert.DoesNotThrow(fun() -> someDataQueue.GetMin |> ignore)
+        someDataQueue.IsEmpty |> Assert.IsFalse
+        Assert.DoesNotThrow(fun() -> someDataQueue.ExtractMin() |> ignore)
+        someDataQueue.IsEmpty |> Assert.IsTrue
