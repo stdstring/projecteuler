8a8c133c11eb86831ab5a80b0964c0b85a20a10d
stdstring <std_string@mail.ru>
2022-11-03 21:43:45 +0500

    added description for problem019, problem020, problem021, problem022, problem023, problem024, problem025

--- projecteuler.fsharp/ProjectEulerTasks/Problem019/Problem019.fs
+++ projecteuler.fsharp/ProjectEulerTasks/Problem019/Problem019.fs
@@ -3,12 +3,6 @@
 open NUnit.Framework
 open ProjectEulerTasks.Utils
 
-// You are given the following information, but you may prefer to do some research for yourself.
-// 1 Jan 1900 was a Monday. Thirty days has September, April, June and November.
-// All the rest have thirty-one, Saving February alone, which has twenty-eight, rain or shine. And on leap years, twenty-nine.
-// A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
-// How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
-
 module Problem019Impl =
     type ProcessData = {LastDay: int; SundaysCount: int}
 
--- projecteuler.fsharp/ProjectEulerTasks/Problem025/Problem025.fs
+++ projecteuler.fsharp/ProjectEulerTasks/Problem025/Problem025.fs
@@ -3,24 +3,6 @@
 open NUnit.Framework
 open ProjectEulerTasks.Utils
 
-// The Fibonacci sequence is defined by the recurrence relation:
-// F(n) = F(n-1) + F(n-2), where F(1) = 1 and F(2) = 1.
-// Hence the first 12 terms will be:
-// F(1) = 1
-// F(2) = 1
-// F(3) = 2
-// F(4) = 3
-// F(5) = 5
-// F(6) = 8
-// F(7) = 13
-// F(8) = 21
-// F(9) = 34
-// F(10) = 55
-// F(11) = 89
-// F(12) = 144
-// The 12th term, F(12), is the first term to contain three digits.
-// What is the first term in the Fibonacci sequence to contain 1000 digits?
-
 module Problem025Impl =
     type FibonacciData = {Prev: bigint; Current: bigint; TermNumber: int}
 
@@ -37,7 +19,7 @@ type Problem025() =
             Some ((nextValue, nextTermNumber), {FibonacciData.Prev = data.Current; FibonacciData.Current = nextValue; FibonacciData.TermNumber = nextTermNumber})
         let _, termNumber = {FibonacciData.Prev = 1I; FibonacciData.Current = 0I; FibonacciData.TermNumber = 0} |>
                             Seq.unfold generator |>
-                            Seq.skipWhile (fun (value, termNumber) -> value < infimumValue) |>
+                            Seq.skipWhile (fun (value, _) -> value < infimumValue) |>
                             Seq.head
         termNumber
 
