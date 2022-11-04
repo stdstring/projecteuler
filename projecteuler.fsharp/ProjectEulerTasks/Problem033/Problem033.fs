29814434652393913e7cb40c19098dd294a4df19
stdstring <std_string@mail.ru>
2022-11-04 16:04:11 +0500

    added description for problem029, problem030, problem031, problem032, problem033

--- projecteuler.fsharp/ProjectEulerTasks/Problem033/Problem033.fs
+++ projecteuler.fsharp/ProjectEulerTasks/Problem033/Problem033.fs
@@ -4,11 +4,6 @@ open NUnit.Framework
 open ProjectEulerTasks.Utils
 open CommonLib
 
-// The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting to simplify it may incorrectly believe that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
-// We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
-// There are exactly four non-trivial examples of this type of fraction, less than one in value, and containing two digits in the numerator and denominator.
-// Find these four fractions.
-
 [<TestFixture>]
 type Problem033() =
 
